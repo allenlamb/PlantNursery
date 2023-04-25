938aa21771c5378b696a34fc1894fea36bf2e9f0
allenlamb <92129161+allenlamb@users.noreply.github.com>
2023-04-24 19:32:42 -0700

    Added Notes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const closeTime = new Date();
 
 openTime.setHours(8, 30, 0);
 closeTime.setHours(17, 0, 0);
+
+//Checks to see if current user time is within business hours
 if (!(nowTime >= openTime && nowTime < closeTime)) {
   openSignal.innerHTML = "CLOSED!";
   openSignal.style.color = "#802F1F";
