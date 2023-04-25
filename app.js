e7b99f3bfbffd9ab6449d8154c162dd37e8aa6f3
allenlamb <92129161+allenlamb@users.noreply.github.com>
2023-04-24 18:07:51 -0700

    SEA 2023 Submission

---
+++ app.js
@@ -0,0 +1,15 @@
+const openSignal = document.getElementById("openOrClosed");
+const shopSymbol = document.getElementById("shopSymbol");
+const nowTime = new Date();
+const openTime = new Date();
+const closeTime = new Date();
+
+openTime.setHours(8, 30, 0);
+closeTime.setHours(17, 0, 0);
+if (!(nowTime >= openTime && nowTime < closeTime)) {
+  openSignal.innerHTML = "CLOSED!";
+  openSignal.style.color = "#802F1F";
+  shopSymbol.classList.remove("fa-shop");
+  shopSymbol.classList.add("fa-shop-lock");
+  shopSymbol.style.color = "#802F1F";
+}
